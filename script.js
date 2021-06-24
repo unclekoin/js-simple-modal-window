e69adf4757efe244e056c0aa149d4318043f2b55
Pavel Koryakin <unclekoin@gmail.com>
2021-06-24 13:23:32 +0300

    feat: finish project

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
+
+btn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
